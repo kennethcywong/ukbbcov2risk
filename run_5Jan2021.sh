56c70ceff6d030d1dc0edb629926210a9c25f7a5
Kenneth C.Y. Wong <kenneth.wong@wellmind.com.hk>
2021-03-04 18:17:27 +0800

    Add the shell scripts to run the R scripts

---
+++ run_5Jan2021.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VER="14Dec2020"
+RUN="5Jan2021"
+
+Rscript analysis_dataset.R --pval TRUE --cohort A --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/df_cohorts_${VER}.RData --metapath /home/kenneth/data/UKBB/covid19/meta/5Jan2021 --cpu 32 --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} > /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortA.log 2>&1
+Rscript analysis_dataset.R --pval TRUE --cohort B --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/df_cohorts_${VER}.RData --metapath /home/kenneth/data/UKBB/covid19/meta/5Jan2021 --cpu 32 --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} > /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortB.log 2>&1
+Rscript analysis_dataset.R --pval TRUE --cohort C --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/df_cohorts_${VER}.RData --metapath /home/kenneth/data/UKBB/covid19/meta/5Jan2021 --cpu 32 --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} > /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortC.log 2>&1
+Rscript analysis_dataset.R --pval TRUE --cohort D --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/df_cohorts_${VER}.RData --metapath /home/kenneth/data/UKBB/covid19/meta/5Jan2021 --cpu 32 --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} > /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortD.log 2>&1
+
+Rscript analysis_shapley.R --cohort A --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/xgb_model --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} >> /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortA.log 2>&1
+Rscript analysis_shapley.R --cohort B --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/xgb_model --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} >> /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortB.log 2>&1
+Rscript analysis_shapley.R --cohort C --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/xgb_model --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} >> /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortC.log 2>&1
+Rscript analysis_shapley.R --cohort D --dspath /home/kenneth/out/UKBB/covid19/rerun_${RUN}/xgb_model --outpath /home/kenneth/out/UKBB/covid19/rerun_${RUN} >> /home/kenneth/out/UKBB/covid19/rerun_${RUN}/cohortD.log 2>&1
